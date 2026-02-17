1a6051d7313c4a3e528427225e9847eb40dc9b1f
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2026-02-17 12:54:23 -0300

    Add estrutura básica do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,57 @@
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import { Card, CardContent } from "../components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "../components/ui/card";
 import { Separator } from "../components/ui/separator";
+import { Plus, List, Check, CircleMinus, SquarePen, Trash, ListTodo, Sigma } from 'lucide-react';
+import { Badge } from "../components/ui/badge";
 
 const Home = () => {
   return (
     <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <Card className="w-lg">
-        <CardContent className="flex gap-2">
-          <Input />
-          <Button variant="outline">Cadastrar</Button>
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button variant="default" className="cursor-pointer"><Plus/> Cadastrar</Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4"/>
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer" variant="default"><List/>Todas</Badge>
+            <Badge className="cursor-pointer" variant="outline"><CircleMinus/>Não finalizadas</Badge>
+            <Badge className="cursor-pointer" variant="outline"><Check/>Concluídas</Badge>
+          </div>
+          <div className="mt-4 border-b">
+            <div className="h-14 flex justify-between items-center border-t">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar React.js</p>
+              <div className="flex items-center gap-4">
+                <SquarePen size="16" className="cursor-pointer"/>
+                <Trash size="16" className="cursor-pointer"/>
+              </div>
+            </div>
+          </div>
         </CardContent>
-        <Separator/>
+
+        <CardFooter className="flex-col">
+          <div className="flex justify-between w-full">
+            <div className="flex gap-2 items-center">
+              <ListTodo size="18"/>
+              <p className="text-xs">Tarefas Concluídas (3/3)</p>
+            </div>
+            <Button variant="outline" className="xs h-7 cursor-pointer"><Trash/>Limpar Tarefas Concluídas</Button>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div className="h-full bg-blue-500 rounded-md" style={{width: "50%"}}></div>
+          </div>
+
+          <div className="flex justify-end items-center w-full mt-2 gap-2">
+            <Sigma size="18"/>
+            <p className="text-xs">3 tarefas no total</p>
+          </div>
+          
+        </CardFooter>
       </Card>
     </main>
   );
